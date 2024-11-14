6b344b95c73ddf9cdf6d89d1c35584be049b3bbd
FajarIndahNA <fajarindah285@gmail.com>
2024-11-14 23:59:11 +0700

    perubahan app.jsx/penambahan router main.jsx/ penambaham BlogCard.jsx dan tampilan akan ada header, button create, 4 blogcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogCard from './components/BlogCard';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -13,9 +14,16 @@ function App() {
          <h1 className='text-white'>React Laravel Blog</h1>
      </div>
      <div className='container'>
-        <div className="d-flex">
+        <div className="d-flex justify-content-between pt-5 mb-4">
           <h4>Blogs</h4>
-          <a href="" className="btn btn-dark">Create</a>
+          <a href="#" className="btn btn-dark">Create</a>
+        </div>
+
+        <div className="row">
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
         </div>
      </div>
     </>
